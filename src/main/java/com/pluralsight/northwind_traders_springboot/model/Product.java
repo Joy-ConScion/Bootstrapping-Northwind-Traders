f5b2d52133d29df02f74c38d5b607258719c07b7
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-16 11:55:28 -0500

    Add corrected code of product model class

--- src/main/java/com/pluralsight/northwind_traders_springboot/model/Product.java
+++ src/main/java/com/pluralsight/northwind_traders_springboot/model/Product.java
@@ -1,10 +1,44 @@
 package com.pluralsight.northwind_traders_springboot.model;
 
-public class Product {
+import jakarta.persistence.*;
 
-    int ProductId;
-    String name;
-    String category;
-    double price;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductId")
+    private int productId;
+
+    @Column(name = "ProductName")
+    private String productName;
+
+    @Column(name = "SupplierID")
+    private String supplierId;
+
+    @Column(name = "CategoryID")
+    private int categoryId;
+
+    @Column(name = "QuantityPerUnit")
+    private int quantityPerUnit;
+
+    @Column(name = "UnitPrice")
+    private BigDecimal unitPrice;
+
+    @Column(name = "UnitsInStock")
+    private int unitsInStock;
+
+    @Column(name = "UnitsOnOrder")
+    private int unitsOnOrder;
+
+    @Column(name = "ReorderLevel")
+    private int reorderLevel;
+
+    @Column(name = "Discontinued")
+    private boolean discontinued;
+
+
 
 }
--- src/main/java/com/pluralsight/northwind_traders_springboot/repository/ProductRepository.java
+++ src/main/java/com/pluralsight/northwind_traders_springboot/repository/ProductRepository.java
@@ -1,6 +1,15 @@
 package com.pluralsight.northwind_traders_springboot.repository;
 
+import com.pluralsight.northwind_traders_springboot.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository {
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategoryID(String track);
+    List<Product> findByProductNameContainingIgnoreCase(String speaker);
+
 }
